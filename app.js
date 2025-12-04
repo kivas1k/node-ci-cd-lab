71527024d0ca7bf4ad9a6ef22379669a1f266715
kivas1k <146351514+kivas1k@users.noreply.github.com>
2025-12-05 00:51:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.status(200);
-    res.send("CI/CD Pipeline Test - Success!");
+    res.send("CI/CD Pipeline Test - Success! V2");
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
